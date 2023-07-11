325cac0448cc029705418d2cde1e5f128bf5226b
Sapiofan <leshalitova@gmail.com>
2023-07-11 19:02:02 +0300

    get list of cards for studying

--- app/src/main/java/com/sapiofan/cards/StudyingActivity.java
+++ app/src/main/java/com/sapiofan/cards/StudyingActivity.java
@@ -20,6 +20,8 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,7 +33,10 @@ import com.sapiofan.cards.services.DatabaseHelper;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class StudyingActivity extends AppCompatActivity {
     private ProgressBar progressBar;
@@ -45,13 +50,21 @@ public class StudyingActivity extends AppCompatActivity {
     private boolean isFrontVisible = true;
     private int recalledCount = 0;
 
+    private Map<Card, Boolean> rememberedWords = new HashMap<>();
+    private Map<Card, Integer> forgotWords = new HashMap<>();
+    private Card[] currentCard = new Card[1];
+
+    private DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.studying);
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         // Initialize views
-        DatabaseHelper databaseHelper = new DatabaseHelper(this);
         CardWord cardWord = databaseHelper.getWordsSize();
 
         progressBar = findViewById(R.id.progressBar);
@@ -66,26 +79,57 @@ public class StudyingActivity extends AppCompatActivity {
         textViewFront.setTextSize(TypedValue.COMPLEX_UNIT_PX, cardWord.getSize());
         textViewBack.setTextSize(TypedValue.COMPLEX_UNIT_PX, cardWord.getSize());
 
-        List<Card> cards = getCardList();
+        Random random = new Random();
+        List<Card> cards = databaseHelper.getAllVisibleCards();
+        for (Card cardFromDB : cards) {
+            rememberedWords.put(cardFromDB, false);
+            forgotWords.put(cardFromDB, 0);
+        }
+
+        currentCard[0] = cards.get(Math.abs(random.nextInt()) % cards.size());
+        textViewFront.setText(currentCard[0].getText());
+        textViewBack.setText(currentCard[0].getTranslation());
+
         int totalCount = cards.size();
         progressBar.setMax(totalCount);
         progressBar.setProgress(recalledCount);
         totalCountTextView.setText(String.valueOf(totalCount));
 
         forgotButton.setOnClickListener(v -> {
-            // Perform action when the "Forgot" button is clicked
+            while (true) {
+                Card recallCard = cards.get(Math.abs(random.nextInt()) % cards.size());
+                if(!rememberedWords.get(recallCard)) {
+                    forgotWords.put(currentCard[0], forgotWords.get(currentCard[0]) + 1);
+                    currentCard[0] = recallCard;
+                    textViewFront.setText(recallCard.getText());
+                    textViewBack.setText(recallCard.getTranslation());
+                    break;
+                }
+            }
         });
 
         rememberButton.setOnClickListener(v -> {
+            rememberedWords.put(currentCard[0], true);
+
             recalledCount++;
             int currentProgress = progressBar.getProgress();
             int maxProgress = progressBar.getMax();
             if (currentProgress < maxProgress) {
                 progressBar.setProgress(currentProgress + 1);
+            } else {
+                handleStudyingResults();
+                // show results of studying
             }
             recalledCountTextView.setText(String.valueOf(recalledCount));
-            textViewFront.setText("New Front");
-            textViewBack.setText("New Back");
+            while (true) {
+                Card recallCard = cards.get(Math.abs(random.nextInt()) % cards.size());
+                if(!rememberedWords.get(recallCard)) {
+                    currentCard[0] = recallCard;
+                    textViewFront.setText(recallCard.getText());
+                    textViewBack.setText(recallCard.getTranslation());
+                    break;
+                }
+            }
         });
 
         // Set card click listener
@@ -97,6 +141,15 @@ public class StudyingActivity extends AppCompatActivity {
             }
             isFrontVisible = !isFrontVisible;
         });
+    }
+
+    private void handleStudyingResults() {
+        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout2);
+        LinearLayout linearLayout = findViewById(R.id.buttonsContainer);
+        constraintLayout.setVisibility(View.GONE);
+        linearLayout.setVisibility(View.GONE);
+        card.setVisibility(View.GONE);
+
     }
 
     @Override
@@ -155,25 +208,5 @@ public class StudyingActivity extends AppCompatActivity {
     public void footerSettingsButtonClicked(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
-    }
-
-    private List<Card> getCardList() {
-        // Retrieve the list of cards from your data source or API call
-        // For demonstration purposes, let's create a dummy list
-        List<Card> cards = new ArrayList<>();
-        cards.add(new Card(1, "Card 1", "Translation 1 Translation 1Translation 1Translation 1Translation 1Translation 1Translation 1Translation 1Translation 1", new Date(), 1, 0));
-        cards.add(new Card(2, "Card 2", "Translation 2", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-        cards.add(new Card(3, "Card 3", "Translation 3", new Date(), 1, 0));
-
-        return cards;
     }
 }
